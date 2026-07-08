e88e35ec265258a67255250fbc448788dfd8c322
distribuidoracastilloyanez-create <distribuidoracastilloyanez@gmail.com>
2026-07-07 21:17:24 -0400

    chore(sw): v24

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ventas-app-cache-v23'; // v23: Analista Clientes - modelo cuenta corriente + 2 calificaciones + buscadores
+const CACHE_NAME = 'ventas-app-cache-v24'; // v24: selector de cliente previo (ahorra lecturas) en Analista de Clientes
 
 // Archivos críticos que componen la aplicación ("App Shell")
 const urlsToCache = [
@@ -179,6 +179,7 @@ self.addEventListener('fetch', event => {
             })
     );
 });
+
 
 
 
